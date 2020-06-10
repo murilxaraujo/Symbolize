4ea33ac93b30565c2c5954505ebb241a579d0726
Murilo Araujo <muernandes@icloud.com>
2020-06-10 11:56:46 -0300

    Initial Commit

---
+++ Symbolize/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Symbolize
+//
+//  Created by Murilo Araujo on 10/06/20.
+//  Copyright © 2020 Murilo Araujo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Symbolize/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Symbolize
+//
+//  Created by Murilo Araujo on 10/06/20.
+//  Copyright © 2020 Murilo Araujo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
